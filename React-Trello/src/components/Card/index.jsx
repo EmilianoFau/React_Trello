96cd39574a7c89cf566222e55e9017c23c4a3ac8
longuigon <longuigon+git@gmail.com>
2024-09-23 09:35:15 -0300

    hola

--- React-Trello/src/components/Card/index.jsx
+++ React-Trello/src/components/Card/index.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
 import Button from '../Button/index.jsx';
 import Styles from './index.module.css';
+import PropTypes from 'prop-types';
 
 function Card ({ data }) {
     return (
         <ul className={Styles.card}>
-            <h3>{data[0].status}</h3>
+            <h3>{data && data.length > 0 ? data[0].status : ''}</h3>
             {data.map(task => (
                     <li key={task.id}>
                         <p>{task.title}</p>
                         <Button />
                     </li>
-            ))};
+            ))}
         </ul>
     )
+}
+
+Card.propTypes = {
+    data: PropTypes.array.isRequired
 }
 
 export default Card;
