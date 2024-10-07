20c863cf7183c10e4fda508538bfff1493a9ecfe
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2024-10-07 09:34:23 -0300

    ñ

--- React-Trello/src/components/Button/index.jsx
+++ React-Trello/src/components/Button/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Styles from './index.module.css';
+import { useTheme } from '../../contexts/theme';
 
 const Button = ( {text, onClick} ) => {
+    const { isDarkMode } = useTheme();
+
     return (
-        <button onClick={onClick}>{text}</button>
+        <button onClick={onClick} className={`${isDarkMode ? Styles['dark-mode'] : ''}`}>{text}</button>
     );
 }
 
